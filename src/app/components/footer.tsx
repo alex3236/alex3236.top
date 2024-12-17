4049219bbae85879be23bcbbab00edfceb074be0
Alex3236 <45303195+alex3236@users.noreply.github.com>
2024-12-17 23:27:23 +0800

    feat: footer tweak for consistency

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -1,14 +1,35 @@
 import React from 'react';
 import { FaGithub } from 'react-icons/fa';
 
-export default function Footer() {
-    return (
-        <div
-            id='icp'
-            className="z-20 fixed mt-6 bottom-0 w-full lg:w-fit lg:bottom-4 lg:left-4 bg-opacity-65 justify-center items-center flex align-middle text-white bg-gray-700"
-        >
-            <a href="https://icp.gov.moe/?keyword=20233236" className='drop-shadow-sm deop-shadow-black mx-2 my-1'>萌ICP备20233236</a> |
-            <a aria-label='Github page of this project' href="https://github.com/alex3236/alex3236.top" className='drop-shadow-sm deop-shadow-black mx-2 my-1'><FaGithub /></a>
-        </div>
-    );
+interface AProps {
+  href: string;
+  children: React.ReactNode;
+}
+
+const A: React.FC<AProps> = ({ href, children }) => {
+  return (
+    <a
+      href={href}
+      className='drop-shadow-sm deop-shadow-black mx-2 my-1 hover:text-sky-200'
+    >
+      {children}
+    </a>
+  );
 };
+
+export default function Footer() {
+  return (
+    <div
+      id='icp'
+      className='z-20 fixed mt-6 bottom-0 px-2 w-full h-fit lg:w-fit lg:top-0 lg:right-4 lg:rounded-full bg-opacity-65 justify-center items-center flex align-middle text-white bg-gray-600'
+    >
+      <A href='https://icp.gov.moe/?keyword=20233236'>萌ICP备20233236</A> |
+      <A
+        aria-label='Github page of this project'
+        href='https://github.com/alex3236/alex3236.top'
+      >
+        <FaGithub />
+      </A>
+    </div>
+  );
+}
